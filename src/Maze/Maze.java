4f04577feb198339c0c9b6fdaa4bb41ebb779b15
Nuno Neto <nunonetinho@gmail.com>
2016-02-29 16:23:55 +0000

    Now with keyboard keys

--- src/Maze/Maze.java
+++ src/Maze/Maze.java
@@ -1,5 +1,7 @@
 package Maze;
 
+import java.util.Scanner;
+
 public class Maze {
 
 	public static void main(String[] args) 
@@ -21,6 +23,35 @@ public class Maze {
 		
 		//Printing Table
 		table.PrintTable();
+		
+		//INITIALIZING SCANNER
+		Scanner s = new Scanner(System.in);
+		char c = s.next().charAt(0);
+		
+		while(c != 'F' && c != 'f')
+		{
+			if(c == 'W' || c == 'w')
+			{
+				System.out.println("Up");
+			}
+			if(c == 'A' || c == 'a')
+			{
+				System.out.println("Left");
+			}
+			if(c == 'S' || c == 's')
+			{
+				System.out.println("Down");
+			}
+			if(c == 'D' || c == 'd')
+			{
+				System.out.println("Right");
+			}
+			c = s.next().charAt(0);
+		}
+		
+		//Closing Scanner and Returning
+		s.close();
+		return;
 	}
 
 }
